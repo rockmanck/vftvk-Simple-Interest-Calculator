2651e4ca2645be71b71d419038e6feaf51e74c1f
rockmanck <rockman.roll@gmail.com>
2021-03-22 21:10:00 +0000

    Implemented business logic

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+
+    var interest = principal * years * (rate / 100);
+    var targetYear = new Date().getFullYear() + parseInt(years);
+
+    var result = document.getElementById("result");
+    result.innerHTML = "If you deposit " + principal + "<br/>" 
+        + "at an interest rate of " + rate + "% <br/>"
+        + "You will receive an amount of " + interest + ",<br/>"
+        + "in the year " + targetYear;
+}
+
+function updateRateValue() {
+    var val = document.getElementById("rate").value;
+    document.getElementById("rateValue").innerHTML = val;
 }
         
