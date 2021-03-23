84902c1557558d1cac4e1d2b5df7ef061cea7fc8
rockmanck <rockman.roll@gmail.com>
2021-03-23 19:37:42 +0000

    Added mock table for the experiment

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 function compute()
 {
+    // valuate inputs
     var principal = document.getElementById("principal").value;
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
 
+    // calculate interest
     var interest = principal * years * (rate / 100);
     var targetYear = new Date().getFullYear() + parseInt(years);
 
+    // output interest and format key inputs
     var result = document.getElementById("result");
     result.innerHTML = "If you deposit <span class='highlight'>" + principal + "</span><br/>" 
         + "at an interest rate of <span class='highlight'>" + rate + "%</span><br/>"
@@ -14,16 +17,19 @@ function compute()
         + "in the year <span class='highlight'>" + targetYear + "</span>";
 }
 
+// sync text value of the rate with the slider
 function updateRateValue() {
     var val = document.getElementById("rate").value;
     document.getElementById("rateValue").innerHTML = val;
 }
 
+// rejects negative principal(amount) values
 function validatePrincipal() {
     var input = document.getElementById("principal")
     var principal = input.value;
-    if (parseInt(principal) <= 0) {
-        alert('Enter a positive number');
+
+    if (principal == '' || parseInt(principal) <= 0) {
+        alert('Enter a positive amount');
         input.focus();
     }
 }
